c091ee3466f6dfb6a9db9fa73b9f1b869995e239
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-08-22 00:50:10 -0300

    adicionando as funcionalidade de credito e debito

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -11,7 +11,7 @@
 
         vm.refresh = function() {
             $http.get(url).then(response => {
-                vm.billingCycle = {};
+                vm.billingCycle = {credits: [{}], debts:[{}]};
                 vm.billingCycles = response;
                 tabs.show(vm, {tabList: true, tabCreate: true});
             });
@@ -35,6 +35,54 @@
         vm.showTabDelete = function(billingCycle) {
             vm.billingCycle = billingCycle;
             tabs.show(vm, {tabDelete: true});
+        }
+
+        vm.update = function() {
+            const updateUrl = `${url}/${vm.billingCycle._id}`;
+            $http.put(updateUrl, vm.billingCycle).then(() => {
+                vm.refresh();
+                msgs.addSuccess("Operação realizada com sucesso!");
+            }).catch((data) => {
+                msgs.addError(data.data.errors);
+            });
+        }
+
+        vm.delete = function() {
+            const deleteUrl = `${url}/${vm.billingCycle._id}`;
+            $http.delete(deleteUrl, vm.billingCycle).then(() => {
+                vm.refresh();
+                msgs.addSuccess("Operação realizada com sucesso!");
+            }).catch((data) => {
+                msgs.addError(data.data.errors);
+            });
+        }
+
+        vm.addCredit = function(index) {
+            vm.billingCycle.credits.splice(index + 1, 0, {});
+        }
+
+        vm.cloneCredit = function(index, {name, value}) {
+            vm.billingCycle.credits.splice(index + 1, 0, {name, value});
+        }
+
+        vm.deleteCredit = function(index) {
+            if(vm.billingCycle.credits.length > 1) {
+                vm.billingCycle.credits.splice(index, 1);
+            }
+        }
+
+        vm.addDebt = function(index) {
+            vm.billingCycle.debts.splice(index + 1, 0, {});
+        }
+
+        vm.cloneDebt = function(index, {name, value, status}) {
+            vm.billingCycle.debts.splice(index + 1, 0, {name, value, status});
+        }
+
+        vm.deleteDebt = function(index) {
+            if(vm.billingCycle.debts.length > 1) {
+                vm.billingCycle.debts.splice(index, 1);
+            }
         }
 
         vm.refresh();

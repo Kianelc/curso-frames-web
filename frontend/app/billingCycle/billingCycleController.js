28e2d709fb91df6a97231b8e6ff728b1f1d6067e
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-08-21 23:25:38 -0300

    adicionando no controller quais tabs quero mostrar

--- frontend/app/billingCycle/billingCycleController.js
+++ frontend/app/billingCycle/billingCycleController.js
@@ -2,20 +2,31 @@
     angular.module("primeiroApp").controller("BillingCycleCtrl", [
         "$http",
         "msgs",
+        "tabs",
         BillingCycleController,
     ]);
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this;
+        const url = 'http://localhost:3003/api/billingCycles';
+
+        vm.refresh = function() {
+            $http.get(url).then(response => {
+                vm.billingCycle = {};
+                vm.billingCycles = response;
+                tabs.show(vm, {tabList: true, tabCreate: true});
+            });
+        }
 
         vm.create = function() {
-            const url = 'http://localhost:3003/api/billingCycles';
 
             $http.post(url, vm.billingCycle).then((response) => {
-                vm.billingCycle = {};
+                vm.refresh();
                 msgs.addSuccess("Operação realizada com sucesso!");
             }).catch((data) => {
                 msgs.addError(data.data.errors);
             });
         }
+
+        vm.refresh();
     }
 })()
